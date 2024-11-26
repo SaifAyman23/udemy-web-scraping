cb318e5d497138490ec77b3e7dfc219684c48a74
Saif Ayman <saifayman3021@gmail.com>
2024-11-27 01:23:06 +0200

    All set!

--- scraper.py
+++ scraper.py
@@ -4,13 +4,11 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 # To scrape the first page only
 driver.get("https://www.udemy.com/courses/development/data-science/")
-
 # Wait for the content to load
 driver.implicitly_wait(10)
 
 # To retrieve all the courses via a parent element
 parent = driver.find_element(By.CLASS_NAME, 'course-list_container__yXli8')
-
 # to retrieve all child elements
 children = parent.find_elements(By.CLASS_NAME, 'course-list_card-layout-container__F2SfZ')
 
