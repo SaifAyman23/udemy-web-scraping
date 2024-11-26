a9ab2235648edb3208d7392a917f2444941433b4
Saif Ayman <saifayman3021@gmail.com>
2024-11-27 01:22:41 +0200

    All set!

--- cleaner.py
+++ cleaner.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 courses_list = []
-
 # To split the courses into individual elements
 with open("data.txt") as file:
     courses_list = file.read().split("================================")
@@ -14,20 +13,16 @@ del courses_list[-1]
 data = []
 for course in courses_list:
     element = []
-    
     # Since the list element is a result of using '\n', we don't need it
     del course[-1]
-    
     if course[-1] == "Bestseller":
             course[-1] = "Yes"
     else:
         course.append(np.nan)
-        
     for piece in course:
         # To remove unwanted data
         if isinstance(piece,str) and (piece in ("Instructor:", "Instructors:", "Current Price", "Original Price", "Current price", "Original price", "") or "Rating:" in piece):
             continue
-        
         element.append(piece)
     data.append([element[0],*element[2:]])
             
